0aa62f3691e733777bb13d54eb07932f5fc55335
binhnm <nmbinh@cmcglobal.vn>
2021-10-15 17:38:07 +0700

    new

--- cypress/constants.ts
+++ cypress/constants.ts
@@ -1,3 +1,4 @@
 // file for constants for testing
 export const baseUrl = 'https://staging-finance.rabbitinternet.com/';
 export const questionPageUrl = 'product/car-insurance/questions';
+export const quotePageUrl = 'product/car-insurance/quotes';
